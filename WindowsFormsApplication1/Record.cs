c194c8348451f66c99cb27d6e59789e3041494ec
Adriaan <a3anvs@gmail.com>
2016-09-16 13:28:17 +0200

    ander stuff

--- WindowsFormsApplication1/Record.cs
+++ WindowsFormsApplication1/Record.cs
@@ -43,7 +43,11 @@ namespace WindowsFormsApplication1
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            var filePath = "C:\\test\\result.wav";
+            var filePath = "";
+            var name = "";
+            InputBox("File Name", "Save file as", ref name);
+            filePath = name;
+            mciSendString("save recsound " + "C:\\test\\" + filePath + ".wav", null, 0, 0);
             mciSendString("save recsound " + filePath, null, 0, 0);
             mciSendString("close recsound ", null, 0, 0);
             MessageBox.Show("File Created");
@@ -53,6 +57,43 @@ namespace WindowsFormsApplication1
         {
             mciSendString("stop recsound", "", 0, 0);
             mciSendString("close recsound", "", 0, 0);
+        }
+        public DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+            form.Text = title;
+            label.Text = promptText;
+
+            textBox.Text = value;
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+            DialogResult dialogResult = form.ShowDialog();
+
+            value = textBox.Text;
+            return dialogResult;
         }
     }
 }
